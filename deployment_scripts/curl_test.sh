c4006bab7e30122091afd3519bb1feb0e40b15df
Vinlock <dak@pvpallday.com>
2018-11-16 11:26:09 -0800

    changed curl test to GET method

--- deployment_scripts/curl_test.sh
+++ deployment_scripts/curl_test.sh
@@ -2,7 +2,7 @@
 
 sleep 20
 
-if [[ $(curl -X POST --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:4000/test") != 200 ]]; then
+if [[ $(curl -X GET --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:4000/test") != 200 ]]; then
     echo "Test Failed" 1>&2
     exit 1
 fi
