453d5a37f62da64e44fd686aa9f9ff823e6636cd
Vinlock <dak@pvpallday.com>
2018-12-21 10:30:58 -0800

    Require Session

--- src/modules/account/accountRouter.js
+++ src/modules/account/accountRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const accountController = require('./controllers/accountController');
 const authorizeMiddleware = require('../../middleware/authorize');
+const requireSession = require('../../middleware/requireSession');
 
 const accountRouter = express.Router();
 
 accountRouter.use(authorizeMiddleware());
+accountRouter.use(requireSession());
 
 accountRouter.get('/profile', accountController.getProfile);
 accountRouter.post('/profile/games/:game', accountController.updateProfileGame);
