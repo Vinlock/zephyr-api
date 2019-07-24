a04f8c8224196a2ddf6a428821167e8ad9305aa6
Vinlock <dak@pvpallday.com>
2019-07-24 04:19:46 -0700

    Updates

--- app/db/index.js
+++ app/db/index.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 const {
   APP_MONGO_HOST,
   APP_MONGO_DATABASE_NAME,
+  APP_MONGO_REPLICA_SET,
 } = process.env;
 
-mongoose.set('useCreateIndex', true);
-mongoose.connect(APP_MONGO_HOST, {
+const connectionOptions = {
   useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoReconnect: true,
   dbName: APP_MONGO_DATABASE_NAME,
-});
+};
+
+const hasReplicaSet = Boolean(APP_MONGO_REPLICA_SET && APP_MONGO_REPLICA_SET.length > 0);
+if (hasReplicaSet) {
+  connectionOptions.replicaSet = APP_MONGO_REPLICA_SET;
+}
+
+mongoose.connect(APP_MONGO_HOST, connectionOptions);
